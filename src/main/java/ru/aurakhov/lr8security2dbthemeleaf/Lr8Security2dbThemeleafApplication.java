86e2dcd53e4825ca1f5f66d5c6636d4a851bf7c9
aurakhov <aurakhovtel@gmail.com>
2023-11-29 16:23:29 +0500

    LR8

--- src/main/java/ru/aurakhov/lr8security2dbthemeleaf/Lr8Security2dbThemeleafApplication.java
+++ src/main/java/ru/aurakhov/lr8security2dbthemeleaf/Lr8Security2dbThemeleafApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Lr8Security2dbThemeleafApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lr8Security2dbThemeleafApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Lr8Security2dbThemeleafApplication.class, args);
+    }
 
 }
--- src/test/java/ru/aurakhov/lr8security2dbthemeleaf/Lr8Security2dbThemeleafApplicationTests.java
+++ src/test/java/ru/aurakhov/lr8security2dbthemeleaf/Lr8Security2dbThemeleafApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Lr8Security2dbThemeleafApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
